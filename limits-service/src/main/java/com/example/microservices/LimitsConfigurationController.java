b568f12b1d73ab4d88aba55ebaaece06c678bbda
Hemant Seth <Hemant.Seth@cpa.texas.gov>
2019-09-01 20:43:46 -0400

    First Commit for Limit Service

---
+++ limits-service/src/main/java/com/example/microservices/LimitsConfigurationController.java
@@ -0,0 +1,17 @@
+package com.example.microservices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+ 
+	@Autowired
+	Configuration configuration;
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		return new LimitConfiguration(configuration.getMinimum(), configuration.getMaximum());
+	}
+}
