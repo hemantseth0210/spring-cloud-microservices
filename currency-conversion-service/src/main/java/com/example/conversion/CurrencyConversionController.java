3a7f6ce12dfa6ccc5296139d9c1aeee3ab112412
Hemant Seth <Hemant.Seth@cpa.texas.gov>
2019-09-02 23:46:05 -0400

    Latest Changes

--- currency-conversion-service/src/main/java/com/example/conversion/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/conversion/CurrencyConversionController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,9 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
 
+	private static final Logger logger = LoggerFactory.getLogger(CurrencyConversionController.class);
+
+	
 	@Autowired
 	CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
 	
@@ -43,7 +48,7 @@ public class CurrencyConversionController {
 		
 		CurrencyConversion response = currencyExchangeServiceProxy.
 				retrieveExchangeValue(from, to);
-		
+		logger.info("{}", response);
 		return new CurrencyConversion(response.getId(), from, to, 
 				response.getConversionMultiple(), quantity, 
 				quantity.multiply(response.getConversionMultiple()), response.getPort());
--- currency-exchange-service/src/main/java/com/example/exchange/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/example/exchange/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.example.exchange;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+
+	private static final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
 	@Autowired
 	Environment environment;
@@ -19,6 +23,7 @@ public class CurrencyExchangeController {
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		logger.info("{}",exchangeValue);
 		return exchangeValue;
 	}
 }
