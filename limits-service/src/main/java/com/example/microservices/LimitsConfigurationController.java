f58df8e724d15a26b66e3fcef048911d30763540
Hemant Seth <Hemant.Seth@cpa.texas.gov>
2019-09-03 00:12:15 -0400

    Adding Hystix Behaviour

--- limits-service/src/main/java/com/example/microservices/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/example/microservices/LimitsConfigurationController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class LimitsConfigurationController {
  
@@ -13,5 +15,15 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMinimum(), configuration.getMaximum());
+	}
+	
+	@GetMapping("/hystrix-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveLimitsConfigurations")
+	public LimitConfiguration retrieveLimitsConfigurations() {
+		throw new RuntimeException("Not Available");
+	}
+	
+	public LimitConfiguration fallbackRetrieveLimitsConfigurations() {
+		return new LimitConfiguration(9, 999);
 	}
 }
